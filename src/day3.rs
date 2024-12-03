bd517bf8d19bb269c368a41a4e726efad5a96e9b
Mateusz Chrzonstowski <mateusz.chrzonstowski@paramount.com>
2024-12-03 23:02:35 +0100

    Solve day 3 task 2 splitting by don't()s and do()s

--- src/day3.rs
+++ src/day3.rs
@@ -1,7 +1,15 @@
 fn main() {
-    dbg!(calculate(
+    dbg!(calculate_with_pausing(
         "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
     ));
+}
+
+fn calculate_with_pausing(input: &str) -> i32 {
+    ("do()".to_owned() + input)
+        .split("don't()")
+        .flat_map(|to_do| (to_do.split("do()")).skip(1))
+        .map(calculate)
+        .sum()
 }
 
 fn calculate(input: &str) -> i32 {
@@ -18,6 +26,16 @@ fn calculate(input: &str) -> i32 {
 
 mod tests {
     use super::*;
+
+    #[test]
+    fn resolves_second() {
+        assert_eq!(
+            calculate_with_pausing(
+                "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
+            ),
+            48
+        );
+    }
 
     #[test]
     fn resolves_first() {
